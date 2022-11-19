d4dd874231cfff72479022e8112fc7537c151c50
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-11-19 07:15:54 +0500

    switch cases

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,23 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+
+    public static void main(String[] args) {
+        isLeapYear(1900);
+    }
+    public static void isLeapYear(int year) {
+        if(year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0){
+                    System.out.println("leap");
+                }else {
+                    System.out.println("not leap");
+                }
+            }else {
+                System.out.println("leap");
+            }
+        }else {
+            System.out.println("not leap");
+        }
     }
 }
